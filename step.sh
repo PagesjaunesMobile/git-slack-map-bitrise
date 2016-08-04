446a4b634d94c9897b59339ccc91e0e07a2df1f7
Michaël Duthilleul <mduthilleul.externe@pagesjaunes.fr>
2016-08-04 16:24:53 +0200

    Add @ in slack name

--- step.sh
+++ step.sh
@@ -6,7 +6,10 @@ if [ -f .slack-names ] && [[ ! -z $GIT_CLONE_COMMIT_AUTHOR_EMAIL ]]; then
   SLACK_NAME=$(grep -E "^$GIT_CLONE_COMMIT_AUTHOR_EMAIL = " .slack-names | awk -F" = " '{print $2}')
   if [[ -z $SLACK_NAME ]]; then
     SLACK_NAME="Slack name not found for user $GIT_CLONE_COMMIT_AUTHOR_EMAIL"
+  else
+    SLACK_NAME="@$SLACK_NAME"
   fi
+  
   echo "SLACK_NAME = $SLACK_NAME"
   envman add --key GIT_CLONE_COMMIT_AUTHOR_SLACK  --value "$SLACK_NAME"
 fi
