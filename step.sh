cc971383112dc8499bd8b8c072fbd8276990a1ea
Michaël Duthilleul <mduthilleul.externe@pagesjaunes.fr>
2016-08-04 11:05:40 +0200

    fix

--- step.sh
+++ step.sh
@@ -2,10 +2,11 @@
 # fail if any commands fails
 set -e
 if [ -f .slack-names ] && [[ ! -z $GIT_CLONE_COMMIT_AUTHOR_EMAIL ]]; then
+  GIT_CLONE_COMMIT_AUTHOR_EMAIL=$(echo $GIT_CLONE_COMMIT_AUTHOR_EMAIL | tr -d '"')
   SLACK_NAME=$(grep -E "^$GIT_CLONE_COMMIT_AUTHOR_EMAIL = " .slack-names | awk -F" = " '{print $2}')
-  SLACK_NAME=$(echo $SLACK_NAME | tr -d '"')
   if [[ -z $SLACK_NAME ]]; then
     SLACK_NAME="Slack name not found for user $GIT_CLONE_COMMIT_AUTHOR_EMAIL"
   fi
+  echo "SLACK_NAME = $SLACK_NAME"
   envman add --key GIT_CLONE_COMMIT_AUTHOR_SLACK  --value "$SLACK_NAME"
 fi
