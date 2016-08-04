2f6920ff1a758e17a41841b6d314cda488a694d5
Michaël Duthilleul <mduthilleul.externe@pagesjaunes.fr>
2016-08-04 10:58:22 +0200

    Remove unecessary quote from git email

--- step.sh
+++ step.sh
@@ -3,6 +3,7 @@
 set -e
 if [ -f .slack-names ] && [[ ! -z $GIT_CLONE_COMMIT_AUTHOR_EMAIL ]]; then
   SLACK_NAME=$(grep -E "^$GIT_CLONE_COMMIT_AUTHOR_EMAIL = " .slack-names | awk -F" = " '{print $2}')
+  SLACK_NAME=$(echo $SLACK_NAME | tr -d '"')
   if [[ -z $SLACK_NAME ]]; then
     SLACK_NAME="Slack name not found for user $GIT_CLONE_COMMIT_AUTHOR_EMAIL"
   fi
