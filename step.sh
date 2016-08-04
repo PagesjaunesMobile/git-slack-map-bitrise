b5f660166bccb238a9aead2753d2e02d634cf44b
Michaël Duthilleul <mduthilleul.externe@pagesjaunes.fr>
2016-08-04 16:25:28 +0200

    Simplify error message

--- step.sh
+++ step.sh
@@ -5,7 +5,7 @@ if [ -f .slack-names ] && [[ ! -z $GIT_CLONE_COMMIT_AUTHOR_EMAIL ]]; then
   GIT_CLONE_COMMIT_AUTHOR_EMAIL=$(echo $GIT_CLONE_COMMIT_AUTHOR_EMAIL | tr -d '"')
   SLACK_NAME=$(grep -E "^$GIT_CLONE_COMMIT_AUTHOR_EMAIL = " .slack-names | awk -F" = " '{print $2}')
   if [[ -z $SLACK_NAME ]]; then
-    SLACK_NAME="Slack name not found for user $GIT_CLONE_COMMIT_AUTHOR_EMAIL"
+    SLACK_NAME="$GIT_CLONE_COMMIT_AUTHOR_EMAIL"
   else
     SLACK_NAME="@$SLACK_NAME"
   fi
