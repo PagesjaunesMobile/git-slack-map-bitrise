0d50cdfe564eba7403b6eae884ad85292b2180a5
Michaël Duthilleul <mduthilleul.externe@pagesjaunes.fr>
2016-08-04 10:10:12 +0200

    Add error message if name not found

--- step.sh
+++ step.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # fail if any commands fails
 set -e
-if [ -f .slack-names ]; then
-  SLACK_NAME=$(grep "$GIT_CLONE_COMMIT_AUTHOR_EMAIL" .slack-names | awk -F" = " '{print $2}')
-  envman add --key GIT_CLONE_COMMIT_AUTHOR_SLACK  --value "$SLACK_NAME"
+if [ -f .slack-names ] && [[ ! -z $GIT_CLONE_COMMIT_AUTHOR_EMAIL ]]; then
+  SLACK_NAME=$(grep -E "^$GIT_CLONE_COMMIT_AUTHOR_EMAIL = " .slack-names | awk -F" = " '{print $2}')
+  if [[ -z $SLACK_NAME ]]; then
+    SLACK_NAME="Slack name not found for user $GIT_CLONE_COMMIT_AUTHOR_EMAIL"
+  fi
+  #envman add --key GIT_CLONE_COMMIT_AUTHOR_SLACK  --value "$SLACK_NAME"
+  echo $SLACK_NAME
 fi
