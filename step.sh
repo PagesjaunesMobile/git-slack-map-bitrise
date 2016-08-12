500186a6faf01fe59923aa8c90ef18fc10b3ec11
Michaël Duthilleul <mduthilleul.externe@pagesjaunes.fr>
2016-08-12 11:31:06 +0200

    Rename .slack-names file to .slackmap

--- step.sh
+++ step.sh
@@ -3,7 +3,7 @@
 set -e
 if [ -f .slack-names ] && [[ ! -z $GIT_CLONE_COMMIT_AUTHOR_EMAIL ]]; then
   GIT_CLONE_COMMIT_AUTHOR_EMAIL=$(echo $GIT_CLONE_COMMIT_AUTHOR_EMAIL | tr -d '"')
-  SLACK_NAME=$(grep -E "^$GIT_CLONE_COMMIT_AUTHOR_EMAIL = " .slack-names | awk -F" = " '{print $2}')
+  SLACK_NAME=$(grep -E "^$GIT_CLONE_COMMIT_AUTHOR_EMAIL = " .slackmap | awk -F" = " '{print $2}')
   if [[ -z $SLACK_NAME ]]; then
     SLACK_NAME="$GIT_CLONE_COMMIT_AUTHOR_EMAIL"
   else
