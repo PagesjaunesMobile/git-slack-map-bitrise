80d2b55af58c40d6bc9bc46a2ae2ae8935ad74c7
Michaël Duthilleul <mduthilleul.externe@pagesjaunes.fr>
2016-08-12 11:42:09 +0200

    Fix

--- step.sh
+++ step.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # fail if any commands fails
 set -e
-if [ -f .slack-names ] && [[ ! -z $GIT_CLONE_COMMIT_AUTHOR_EMAIL ]]; then
+if [ -f .slackmap ] && [[ ! -z $GIT_CLONE_COMMIT_AUTHOR_EMAIL ]]; then
   GIT_CLONE_COMMIT_AUTHOR_EMAIL=$(echo $GIT_CLONE_COMMIT_AUTHOR_EMAIL | tr -d '"')
   SLACK_NAME=$(grep -E "^$GIT_CLONE_COMMIT_AUTHOR_EMAIL = " .slackmap | awk -F" = " '{print $2}')
   if [[ -z $SLACK_NAME ]]; then
