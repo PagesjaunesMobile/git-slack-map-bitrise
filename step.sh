5c7413ad58678ffb7925bc775a8bf7877ac073d4
Michaël Duthilleul <mduthilleul.externe@pagesjaunes.fr>
2016-08-18 15:19:48 +0200

    Set global default name to email

--- step.sh
+++ step.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # fail if any commands fails
 set -e
+SLACK_NAME="$GIT_CLONE_COMMIT_AUTHOR_EMAIL"
 if [ -f .slackmap ] && [[ ! -z $GIT_CLONE_COMMIT_AUTHOR_EMAIL ]]; then
   GIT_CLONE_COMMIT_AUTHOR_EMAIL=$(echo $GIT_CLONE_COMMIT_AUTHOR_EMAIL | tr -d '"')
   SLACK_NAME=$(grep -E "^$GIT_CLONE_COMMIT_AUTHOR_EMAIL = " .slackmap | awk -F" = " '{print $2}')
@@ -9,7 +10,7 @@ if [ -f .slackmap ] && [[ ! -z $GIT_CLONE_COMMIT_AUTHOR_EMAIL ]]; then
   else
     SLACK_NAME="@$SLACK_NAME"
   fi
-  
-  echo "SLACK_NAME = $SLACK_NAME"
-  envman add --key GIT_CLONE_COMMIT_AUTHOR_SLACK  --value "$SLACK_NAME"
 fi
+echo "SLACK_NAME = $SLACK_NAME"
+envman add --key GIT_CLONE_COMMIT_AUTHOR_SLACK  --value "$SLACK_NAME"
+
